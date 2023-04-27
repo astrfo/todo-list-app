fc76d14670d98d1cb4af6672c68f2b92ce50555e
Jun <kumejun2000@gmail.com>
2023-04-27 11:56:18 +0900

    テキストフィールド，ボタンの追加

--- TodoListApp.java
+++ TodoListApp.java
@@ -4,11 +4,29 @@ import java.awt.event.*;
 import java.util.List;
 
 public class TodoListApp extends JFrame implements ActionListener {
+    private JTextField inputField;
+    private JButton addButton, removeButton;
 
     public TodoListApp() {
         setTitle("Todo App");
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        inputField = new JTextField();
+
+        addButton = new JButton("追加");
+        addButton.addActionListener(this);
+
+        removeButton = new JButton("削除");
+        removeButton.addActionListener(this);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.add(inputField);
+        inputPanel.add(addButton);
+        inputPanel.add(removeButton);
+
+        setLayout(new BorderLayout());
+        add(inputPanel, BorderLayout.SOUTH);
 
         setVisible(true);
     }
